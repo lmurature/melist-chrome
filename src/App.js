2cb19942520ea6ddf3d4ecf05426a354d1468a66
lmurature <lucio.murature@mercadolibre.com>
2021-09-09 14:15:25 -0300

    initial commit. initial functionality. using stored refresh token cookie on melist production app, getting access token from api and getting user's data. Also get current tab URL

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+function App(props) {
+  const { refreshToken, tabUrl } = props;
+
+  const [userData, setUserData] = useState(null);
+  const [accessToken, setAccessToken] = useState("");
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .post("https://melist-api.herokuapp.com/api/users/auth/refresh_token", {
+        refresh_token: refreshToken,
+      })
+      .then((response) => {
+        let at = response.data.access_token;
+        setAccessToken(at);
+        axios
+          .get("https://melist-api.herokuapp.com/api/users/me", {
+            headers: {
+              Authorization: "Bearer " + at,
+            },
+          })
+          .then((response) => setUserData(response.data))
+          .catch((err) => setError(err));
+      })
+      .catch((err) => setError(err));
+  }, refreshToken);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      Hola! {userData && userData.first_name + "!"}
+      <br />
+      {tabUrl && "estás en la pestaña con URL " + tabUrl}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*global chrome*/
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+chrome.cookies.get(
+  { url: "https://melist-app.herokuapp.com/summary", name: "refresh-token" },
+  (cookie) => {
+    if (cookie) {
+      console.log(cookie.value);
+
+      chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+        ReactDOM.render(
+          <React.StrictMode>
+            <App refreshToken={cookie.value} tabUrl={tabs[0].url} />
+          </React.StrictMode>,
+          document.getElementById("root")
+        );
+      });
+    } else {
+      ReactDOM.render(
+        <React.StrictMode>
+          <div>Inicia sesión en ME List para utilizar la extensión</div>
+        </React.StrictMode>,
+        document.getElementById("root")
+      );
+    }
+  }
 );
 
 // If you want to start measuring performance in your app, pass a function
